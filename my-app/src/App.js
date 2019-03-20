bbeb57cf870c25036c4b063ee6bc5d60b24a69cd
unknown <risnarendra@gmail.com>
2019-03-19 19:13:36 -0500

    Finished left component of website

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div class="WebsiteTopics">
+        <div class="MyName">
+          Rishab Narendra
+        </div>
+
+        <div class="Tabs">
+          <p class="TabLinks">Bio</p>
+          <p class="TabLinks">Work</p>
+          <p class="TabLinks">Projects</p>
+          <p class="TabLinks">Contact</p>
+        </div>
+
+        <div class="Introduction">
+          <span class="Intro">Hey, I'm <br></br></span>
+          <span class="BiggerMyName">Rishab <br></br></span>
+          <span class="BiggerMyName"> Narendra <br></br></span>
+          <span class="SmallerMyName">Iowa State University / Computer Science</span>
+        </div>
       </div>
     );
   }
