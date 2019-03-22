e825860690dcbbd934001e5f926ec209a205949a
unknown <risnarendra@gmail.com>
2019-03-21 23:38:40 -0500

    Finished implementing contact me page

--- my-app/src/components/contact.js
+++ my-app/src/components/contact.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react'; 
 
 class contact extends Component{
+    test(){
+        alert('works');
+    }
+
     render(){
         return(
             <div class="Summary">
                 <h1 class="Contact">Contact Me</h1>
                 <form class="ContactForm">
-                    <p class="Question">Hi, Wave emoji What's your name</p>
-                    <label for="fname">First Name</label>
-                    <input type="text" id="fname" name="firstname" placeholder="Your name.."></input>
-
-                    <label for="lname">Last Name</label>
-                    <input type="text" id="lname" name="lastname" placeholder="Your last name.."></input>
+                    <label class="Label">NAME</label>
+                    <input type="text" class="ContactInput" size="100"></input>
+                    <br></br><br></br>
+                    <label class="Label">EMAIL</label>
+                    <input type="text" class="ContactInput" size="100"></input>
+                    <br></br><br></br>
+                    <label class="Label">PHONE</label>
+                    <input type="text" class="ContactInput" size="100"></input>
+                    <br></br><br></br>
+                    <label class="Label">MESSAGE</label>
+                    <textarea class="ContactInput" name="message" spellcheck="false"></textarea>
+                    <br></br><br></br>
+                    <button class="Submit" onClick={() => this.test()}>Send Message</button>
                 </form>
             </div>
         );
