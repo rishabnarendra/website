d5f61ed2b368b758771305da9cfb6617e77f4c7d
unknown <risnarendra@gmail.com>
2019-05-21 23:39:12 -0500

    Added descriptions for project

--- my-app/src/components/projects.js
+++ my-app/src/components/projects.js
@@ -6,23 +6,17 @@ class projects extends Component{
             <div class="Summary">
                 <h1 class="Projects">Projects</h1>  
                 <p>
-                    <h3>911 NEXT GENERATION</h3>
+                    <h3>NextGen911</h3>
                     <p class="Description">
-                        Android and Windows application to assist 911 callers by providing operators with important information
-                        such as caller location, blood type, and other relevant information. When an operator receives caller 
-                        information they may view nearby law enforcement, fire, and emergency medical services to deploy based
-                        on callers location and condition. 
-                    </p>
-                    <br></br>
-                    <h3>RUBIK'S CUBE SOLVER</h3>
-                    <p class="Description">
-                        Java program to solve a 3*3 Rubik's Cube in under a second.  
+                    An Android app that uses publicly available emergency services in close proximity to a caller’s location to 
+                    allow faster response time during 911 emergencies. Desktop operator receives caller’s health details for 
+                    faster aid.
                     </p>
                     <br></br>
                     <h3>SALT ANALYSIS SIMULATOR</h3>
                     <p class="Description">
-                        C++ project that simulates salt analysis using preliminary, dilute acid, concentrated acid, and reagent 
-                        tests.
+                    An algorithmic C++ program that simulates the lab salt analysis process by performing preliminary, dilute acid, 
+                    concentrated acid, and reagent tests.
                     </p>
                 </p>               
             </div>
