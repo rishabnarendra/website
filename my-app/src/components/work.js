bd9581810b7daae4c73fb9df1979635fbe649d06
unknown <risnarendra@gmail.com>
2019-05-21 23:26:25 -0500

    Edited work information section with project outcomes

--- my-app/src/components/work.js
+++ my-app/src/components/work.js
@@ -6,17 +6,30 @@ class work extends Component{
             <div class="Summary">
                 <h1 class="Work">Work</h1> 
                 <p>
-                    <h3>SOFTWARE INTERN/RESEARCH ASSISTANT</h3>
-                    <h3>Center for Statistics and Applications in Forensic Evidence</h3>
-                    <p></p>
+                    <h3>SOFTWARE DEVELOPMENT ENGINEER INTERN, FARM BUREAU FINANCIAL SERVICES</h3>
+                    <h3 class="Dates">MAY 2019 - AUG 2019</h3>
+                    <p>Architected and built a self service tool for Claims teams to automate payments and receipts</p>
+                    <p>Replaced manual, repetitive processes; reduced business costs for the company</p>
+                    <p>Used C#, SQL, HTML/CSS, JavaScript, AA Enterprise Client, and Jenkins</p>
                     <br></br>
-                    <h3>SOFTWARE INTERN</h3>
-                    <h3>Farm Bureau Financial Services</h3>
-                    <p></p>
+                    <h3>SOFTWARE DEVELOPMENT ENGINEER INTERN, CSAFE</h3>
+                    <h3 class="Dates">SEP 2018 - DEC 2018</h3>
+                    <p>Designed a web application to interact with X3P (XML 3D Surface Profiles) files</p>
+                    <p>Created an HTTP REST API for abstracting interactions</p>
+                    <p>Used Java, C#, SQL, HTML/CSS, and JavaScript</p>
                     <br></br>
-                    <h3>TEACHING ASSISTANT</h3>
-                    <h3>Iowa State University</h3>
-                    <p></p>
+                    <h3>SOFTWARE DEVELOPMENT ENGINEER INTERN, FARM BUREAU FINANCIAL SERVICES</h3>
+                    <h3 class="Dates">MAY 2018 - AUG 2018</h3>
+                    <p>Designed and built a mobile application (iOS & Android) for Marketing and Distribution teams</p>
+                    <p>Provides easy access for insurance customers information and quick payments</p>
+                    <p>Application published on Google Play and App Store</p>
+                    <p>Used C#, SQL, HTML/CSS, TypeScript, Ionic, and Jenkins</p>
+                    <br></br>
+                    <h3>TEACHING INSTRUCTOR, IOWA STATE UNIVERSITY</h3>
+                    <h3 class="Dates">JAN 2018 - MAY 2018</h3>
+                    <p>Teaching Instructor for Introduction to Web Programming</p>
+                    <p>Responsibilities include teaching fundamental web design concepts in HTML/CSS and JavaScript, 
+                    <br></br>managing course infrastructure, and mentoring course students through semester projects</p>
                 </p>               
             </div>
         );
